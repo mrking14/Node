bb572b38cba05f9b54c17be7ac3a8fa079f34233
Masoom Raza <masoomraza843113@outlook.com>
2024-07-01 20:51:56 -0700

    .........

---
+++ node_practice/script.js
@@ -0,0 +1,12 @@
+import { add, sub } from "./math";
+
+require /math
+
+let a =10;
+let b = 2;
+console.log(a+b);
+let subs = sub(a,b);
+let adds = add(a,b);
+
+console.log(adds);
+console.log(subs);
