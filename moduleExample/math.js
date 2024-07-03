090afa48cccf4aa0f3f9b51085981c0700a520de
Masoom Raza <masoomraza843113@outlook.com>
2024-07-03 22:16:04 +0530

    first node module

---
+++ moduleExample/math.js
@@ -0,0 +1,19 @@
+function add(a,b) {
+    return a+b; 
+};
+
+function subs (a,b) {
+    return a-b;
+};
+
+function mult(a,b) {
+    return a*b;
+};
+
+function div(a,b){
+    return a/b;
+};
+
+exports ={
+    add,subs,mult,div
+};
---
+++ moduleExample/script.js
@@ -0,0 +1,5 @@
+let {add,subs,mult,div} = require ("./math.js");
+console.log("Masoom raza",add(10,5)) 
+console.log("Masoom raza",subs(10,5)) 
+console.log("Masoom raza",mult(10,5)) 
+console.log("Masoom raza",div(10,5)) 
